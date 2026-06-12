240a938ed89d7d5c60306f009ccc488d9ee6881d
Katsu <151453865+KatsuReal@users.noreply.github.com>
2026-06-12 21:16:01 +0100

    Add files via upload

---
+++ config/custom-formats.ts
@@ -0,0 +1,13 @@
+
+
+export const Formats: FormatList = [
+    {
+        section: "Custom Mons",
+    },
+    {
+		name: "Custom Mon",
+		desc: `Custom Mons!`,
+		mod: 'custommons',
+		ruleset: ['Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod'],
+	},
+];
